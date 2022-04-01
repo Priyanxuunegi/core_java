0a75a8eb3e63449a04b0fe0356f05515cf793195
Priyanxuunegi <91242105+Priyanxuunegi@users.noreply.github.com>
2022-04-01 16:12:32 +0530

    keyword

--- Java_project_Priyanshu/src/unit_01/P2_task01_keyword_InJava.java
+++ Java_project_Priyanshu/src/unit_01/P2_task01_keyword_InJava.java
@@ -3,8 +3,11 @@ package unit_01;
 public class P2_task01_keyword_InJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int char = 5;   // Syntax error on token "char", invalid VariableDeclaratorId
+		
+		System.out.println(char);
+		
 	}
 
 }
