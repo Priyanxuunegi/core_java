2b9fe07a6b548b8027bbfab7934995ed1f87755d
Priyanxuunegi <91242105+Priyanxuunegi@users.noreply.github.com>
2022-04-01 16:07:30 +0530

    first program

---
+++ Java_project_Priyanshu/src/unit_01/P1_task01_FirrstProgramInJava.java
@@ -0,0 +1,10 @@
+package unit_01;
+
+public class P1_task01_FirrstProgramInJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hello java ");
+	}
+
+}
