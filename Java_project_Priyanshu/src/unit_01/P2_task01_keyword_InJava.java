411c082790c24165f4aa45a2e2f256988a67be92
Priyanxuunegi <91242105+Priyanxuunegi@users.noreply.github.com>
2022-04-01 16:21:31 +0530

    P2

--- Java_project_Priyanshu/src/unit_01/P2_task01_keyword_InJava.java
+++ Java_project_Priyanshu/src/unit_01/P2_task01_keyword_InJava.java
@@ -1,16 +1,35 @@
+
+
+
+
+
 package unit_01;
 
-public class P2_task01_keyword_InJava {
+public class P2_task01_keyword_InJava{
 
 	public static void main(String[] args) {
+		//variable name
+		//Syntax error on token "double", invalid VariableDeclaratorId
+		//identifier can never be any keyword
+		int double=20;
 		
-		int char = 5;   // Syntax error on token "char", invalid VariableDeclaratorId
-		
-		System.out.println(char);
-		int a = 10 ; 
-		System.out.println(a);
-		// PRIYANSHU NEGI 
-		
+		//function name
+		//Syntax error on token "new", invalid AnnotationName
+		//identifier can never be any keyword
+		int new() {
+			
+			//sysout[ctrl+space] is the shorcut key for this command
+			System.out.println();
+			return 0;
+		}
+       //keyword can never be used as object name
+	//ABC cannot be resolved to a variable
+     //Syntax error on token "int", delete this token 
+	 ABC int=new ABC;
+	 
+	 //class object can be created inside main even when it's non static
+	 ABC n=new ABC();
+	 n.display();
 	}
 
 }
